8ad496c0859cb80cb0ab8c59a399811d6a3e9ed9
SHS-Peb <blackskiesdancer@gmail.com>
2025-12-25 00:16:02 +1100

    Build site into docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/beryl-jiang-portfolio/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
 });
